ca8e95287cba805f5d6fd2e245f00da09db503f1
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-05-08 19:48:43 -0600

    Switch to black-box testing

--- internal/protocol/protocol_test.go
+++ internal/protocol/protocol_test.go
@@ -1,36 +1,41 @@
-package protocol
+package protocol_test
 
 import (
 	"bytes"
 	"reflect"
+	"strings"
 	"testing"
-	"unicode/utf16"
+
+	"github.com/richgrov/oneworld/internal/protocol"
 )
 
 type TestPacket struct {
 	Field1 byte
 	Field2 int32
 	Field3 int64
-	Field4 string `maxLen:"5"`
+	Field4 string `maxLen:"64"`
 }
 
 func TestRead(t *testing.T) {
+	// Packet should be read OK
 	const packetId = 2
 	packet := &TestPacket{}
-	encoded := bytes.NewBuffer(Marshal(packetId, packet))
+	encoded := bytes.NewBuffer(protocol.Marshal(packetId, packet))
 
 	var decoded TestPacket
-	if err := ReadPacket(encoded, packetId, &decoded); err != nil {
+	if err := protocol.ReadPacket(encoded, packetId, &decoded); err != nil {
 		t.Errorf("decode failed: %s", err)
 	}
 
-	encoded = bytes.NewBuffer(Marshal(packetId, packet))
-	if err := ReadPacket(encoded, packetId+1, &decoded); err == nil {
+	// Packet with unexpected ID should return error
+	encoded = bytes.NewBuffer(protocol.Marshal(packetId, packet))
+	if err := protocol.ReadPacket(encoded, packetId+1, &decoded); err == nil {
 		t.Errorf("decode succeeded on invalid packet ID: %+v", &decoded)
 	}
 }
 
 func TestMarshal(t *testing.T) {
+	// Packet should be encoded/decoded OK
 	const packetId = 2
 	packet := &TestPacket{
 		Field1: 10,
@@ -39,7 +44,7 @@ func TestMarshal(t *testing.T) {
 		Field4: "hello",
 	}
 
-	encoded := bytes.NewBuffer(Marshal(packetId, packet))
+	encoded := bytes.NewBuffer(protocol.Marshal(packetId, packet))
 
 	if b, err := encoded.ReadByte(); err != nil {
 		t.Fatalf("error reading byte: %s", err)
@@ -48,7 +53,7 @@ func TestMarshal(t *testing.T) {
 	}
 
 	var decoded TestPacket
-	if err := Unmarshal(encoded, &decoded); err != nil {
+	if err := protocol.Unmarshal(encoded, &decoded); err != nil {
 		t.Fatalf("unmarshal failed: %s", err)
 	}
 
@@ -57,47 +62,40 @@ func TestMarshal(t *testing.T) {
 	}
 }
 
-func TestStringMax(t *testing.T) {
-	packet := &TestPacket{
-		Field4: "aaaaaa",
-	}
-
-	encoded := bytes.NewBuffer(Marshal(0, packet))
-	encoded.ReadByte()
-
-	var decoded TestPacket
-	if err := Unmarshal(encoded, &decoded); err == nil {
-		t.Errorf("unmarshal should return error, but got '%s'", decoded.Field4)
-	}
-}
-
 func TestStrings(t *testing.T) {
-	strings := []string{
+	// Strings should be encoded/decoded OK
+	testStrings := []string{
 		"hello", "World", "!", "", "I\u2665Special\ufe4fSymbols",
 	}
 
-	for _, str := range strings {
-		buf := bytes.NewBuffer(make([]byte, 0))
-		if err := writeString(buf, str); err != nil {
-			t.Fatalf("writing '%s' to local buffer returned error: %s", str, err)
+	for _, str := range testStrings {
+		packet := &TestPacket{
+			Field4: str,
 		}
 
-		numCodeUnits := len(utf16.Encode([]rune(str)))
+		encoded := bytes.NewBuffer(protocol.Marshal(0, packet))
+		encoded.ReadByte()
 
-		decoded, err := readString(buf, uint16(numCodeUnits))
-		if err != nil {
-			t.Fatalf("reading encoded string '%s' failed: %s", str, err)
+		var decoded TestPacket
+		if err := protocol.Unmarshal(encoded, &decoded); err != nil {
+			t.Errorf("unmarshal error: %s", err)
 		}
 
-		if str != decoded {
-			t.Errorf("decoded string '%s' doesn't match encoded '%s'", decoded, str)
+		if !reflect.DeepEqual(packet, &decoded) {
+			t.Errorf("packets not equal:\n%+v\n%+v", packet, &decoded)
 		}
 	}
 
-	buf := bytes.NewBuffer(make([]byte, 0))
-	writeString(buf, "four")
-	str, err := readString(buf, 2)
-	if err == nil {
-		t.Errorf("decoding too-long string should have returned error, but got '%s'", str)
+	// String that is too long should return error
+	packet := &TestPacket{
+		Field4: strings.Repeat("a", 100),
+	}
+
+	encoded := bytes.NewBuffer(protocol.Marshal(0, packet))
+	encoded.ReadByte()
+
+	var decoded TestPacket
+	if err := protocol.Unmarshal(encoded, &decoded); err == nil {
+		t.Errorf("unmarshal should return error, but got '%s'", decoded.Field4)
 	}
 }
