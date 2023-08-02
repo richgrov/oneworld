b64397f037d9573bee6e0a73078e3794ef7ba4ae
Richard Grover <38992201+richgrov@users.noreply.github.com>
2023-08-02 16:25:02 -0600

    Fix trait usage in example

--- example/main.go
+++ example/main.go
@@ -3,25 +3,11 @@ package main
 import (
 	"github.com/richgrov/oneworld"
 	"github.com/richgrov/oneworld/level"
-	"github.com/richgrov/oneworld/traits"
 )
 
-type ServerTrait struct{}
 
-func (t *ServerTrait) OnPlayerJoin(event *oneworld.PlayerJoinEvent) {
-	println(event.Player.Username(), "logged in")
-	traits.Set(event.Player, &PlayerTrait{})
-}
 
-type PlayerTrait struct{}
 
-func (t *PlayerTrait) OnChat(event *oneworld.ChatEvent) {
-	event.Player.Message(event.Player.Username() + ": " + event.Message)
-}
-
-func (t *PlayerTrait) OnCommand(event *oneworld.CommandEvent) {
-	event.Player.Message("/" + event.Command)
-}
 
 func main() {
 	listener, err := oneworld.NewListener("localhost:25565")
@@ -48,6 +34,4 @@ func main() {
 
 		server.Tick()
 	}
-
-	traits.Set(server, &ServerTrait{})
 }
